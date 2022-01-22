ac0fe8c32e27528d119bce656cfcc3c5f15fe3a5
nicolasbernardes <nicolas.beltrao69@gmail.com>
2022-01-22 18:17:46 -0300

    ..

--- index_express.js
+++ index_express.js
@@ -1,17 +1,18 @@
 const res = require("express/lib/response");
-
-const express = requiere("express")
+const path = require("path");
+const express = requiere("express");
 const app = express();
 const PORT = 8080;
 
 app.get("/", (req, res, next) => {
     console.log(req.params);
-    res.send("hola desde express")
-})
+    res.sendFile(path.join(__dirname + "/index.js"));
+});
 
 
 const server = app.listen(PORT, () => {
     console.log(`server on http://localhost:${PORT}`)
-})
+});
 
-server.on("error", error => console.log(error))
+server.on("error", error => console.log(error));
+
